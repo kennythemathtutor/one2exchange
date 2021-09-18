88fc254e1c0891acaef9b25b2acf8b51d08b419b
kennythemathtutor <kennythemathtutor@gmail.com>
2021-09-18 15:21:54 +0800

    initial code

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,14 +7,14 @@ function App() {
   return (
     <div className="App">
       <Symfoni autoInit={true}>
-        <div className="min-h-screen bg-gray-800">
-          <div className="max-w-7xl mx-auto sm:px-6 lg:px-8 ">
-            <div className="text-gray-100 text-6xl pt-28 pb-10">
-              Swappity Swap
+        <div className="min-h-screen bg-gradient-to-bl from-yellow-200 via-yellow-400 to-yellow-700">
+          <div className="border-none max-w-7xl mx-auto sm:px-6 lg:px-8 ">
+            <div className="text-black text-6xl pt-28 pb-10">
+              One 2 Exchange 
             </div>
             <Swap
-              tokenA="0x4C5EB1291EE7982F5562aBA87Dc459f5e76EfdED"
-              tokenB="0x00D3B14F6FE51DCb05b0382D0a5E75e17e1D8EAd"
+              tokenA="0x0F0D09DE61A2FDb2aF13DD5d2E8465C2849C313C"
+              tokenB="0xDF47095bCB61953CE9De559bDA5CC77D20B6e294"
             ></Swap>
           </div>
         </div>
--- frontend/src/components/Swap.tsx
+++ frontend/src/components/Swap.tsx
@@ -95,41 +95,48 @@ export const Swap: React.FC<Props> = ({ tokenA, tokenB }) => {
   };
 
   return (
-    <div className="bg-white shadow sm:rounded-lg">
-      <div className="px-4 py-5">
+    <div className="bg-white shadow sm:rounded-lg border-2 w-7/12 mx-auto">
+      <div className="px-4 py-5 border-none border-red-100">
         <div className="grid grid-cols-3 gap-4">
           <div className="text-gray-800 text-4xl">{tokenASymbol}</div>
           <div className="text-gray-800 text-3xl"> to </div>
           <div className="text-gray-800 text-4xl">{tokenBSymbol}</div>
-          <div className="flex justify-center">
-            <span className="flex-item text-gray-800 text-2xl">Amount:</span>
+          <div className="flex justify-center border-none border-red-500">
+            <div className="border-none mx-auto w-screen ">
+            <span className="flex-item text-gray-800 text-xl float-left ml-3 mt-2">Amount:</span>
             <input
               type="text"
               name="Amount"
               id="amount"
-              className="mx-2 flex-item shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block  border-gray-300 rounded-md text-gray-800 text-2xl w-1/6 text-center"
+              className=" shadow-lg px-2 py-2 bg-gray-200 float-right border-none w-24 mx-2 flex-item  focus:ring-indigo-500 focus:border-indigo-500 block  border-gray-300 rounded-md text-gray-800 text-2xl  text-center"
               placeholder="20"
               onChange={handleAmountChange}
             />
+            </div>
           </div>
           <div></div>
-          <div className="flex justify-center">
-            <span className="flex-item text-gray-800 text-2xl">Receive:</span>
+          <div className="flex justify-center border-none">
+           <div className="border-none mx-auto w-screen "> 
+            <span className="flex-item text-gray-800 text-xl ml-3 mt-2 float-left">Receive:</span>
             <input
               type="text"
               name="Receive"
               id="receive"
               disabled
-              className="mx-2 flex-item shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block  border-gray-300 rounded-md text-gray-800 text-2xl w-1/6 text-center"
+              className="px-2 py-2 bg-gray-200 shadow-lg float-right border-none w-24 mx-2 flex-item  focus:ring-indigo-500 focus:border-indigo-500 block  border-gray-300 rounded-md text-gray-800 text-2xl  text-center"
               placeholder="20"
               value={exchangeAmount}
             />
+            </div>
           </div>
           <div></div>
           <div></div>
+          
+        </div>
+        <div className="border-none mx-auto clear-both">
           <button
             type="submit"
-            className="mt-3 inline-flex items-center justify-center px-4 py-2 border border-transparent shadow-sm font-medium rounded-md text-white bg-gray-800 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+            className="w-44 border-none mx-auto mt-3 inline-flex items-center justify-center px-4 py-2  border-transparent shadow-sm font-medium rounded-md text-white bg-gradient-to-bl from-yellow-200 via-yellow-400 to-yellow-700 "
             onClick={handleSwap}
           >
             Swap!
